ac263b841e2ad279aeeef1d603c54466679b1514
OJWatson <oj.watson@hotmail.co.uk>
2023-03-07 14:24:20 +0000

    Rt scenerio plot in

--- src/run_simulations/plotting.R
+++ src/run_simulations/plotting.R
@@ -27,7 +27,7 @@ vacc_allocation_plot <- function(scenarios, scenario_objects, fit) {
     complete(date = seq.Date(min(date), max(df$date), 1)) %>%
     fill(doses, .direction = "down") %>%
     filter(date > as.Date("2020-01-08")) %>%
-    ggplot(aes(x = as.integer(date - as.Date("2020-04-20"))+100, doses, color = scenario)) +
+    ggplot(aes(x = as.integer(date - cepi_start_date)+100, doses, color = scenario)) +
     geomtextpath::geom_textvline(label = "100-Day Mission Target", xintercept = 100, hjust = 0.59) +
     geom_line(lwd = 1) +
     ggpubr::theme_pubclean(base_size = 14) +
@@ -43,7 +43,7 @@ vacc_allocation_plot <- function(scenarios, scenario_objects, fit) {
     complete(date = seq.Date(min(date), max(df$date), 1)) %>%
     fill(doses, .direction = "down") %>%
     filter(date > as.Date("2020-01-08")) %>%
-    ggplot(aes(x = as.integer(date - as.Date("2020-04-20"))+100, doses, color = scenario)) +
+    ggplot(aes(x = as.integer(date - cepi_start_date)+100, doses, color = scenario)) +
     geomtextpath::geom_textvline(label = "100-Day Mission Target", xintercept = 100, hjust = 0.59) +
     geom_line(lwd = 1) +
     ggpubr::theme_pubclean(base_size = 14) +
@@ -54,5 +54,44 @@ vacc_allocation_plot <- function(scenarios, scenario_objects, fit) {
     ylab("Daily Primary Vaccine Doses")
 
   cowplot::plot_grid(gg1, gg2, ncol = 1, align = "v")
+
+}
+
+rt_scenario_plot <- function(scenarios, scenario_objects, fit) {
+
+  # create our vaccine allocation dataframe
+  rt_list <- list()
+  for(i in seq_along(unique(scenarios$Rt))) {
+    ii <- match(unique(scenarios$Rt), scenarios$Rt)[i]
+    rt <- squire.page:::get_Rt(scenario_objects[[ii]])
+    rt$scenario <- scenarios$Rt[ii]
+    rt <-
+    rt_list[[i]] <- rt
+  }
+  rt_list[[length(rt_list) + 1]] <- squire.page:::get_Rt(fit)
+  rt_list[[length(rt_list)]]$scenario <- "baseline"
+
+  # bind and summarise
+  df <- do.call(rbind, rt_list)
+  df <- df %>%
+    filter(date > as.Date("2020-01-08")) %>%
+    group_by(scenario, date) %>%
+    summarise(Rt = median(Rt))
+
+  # Demonstration Plot of it all together
+  gg <- df %>%
+    ggplot(aes(x = as.integer(date - cepi_start_date) + 100, Rt, color = scenario)) +
+    geomtextpath::geom_textvline(label = "100-Day Mission Target", xintercept = 100, hjust = 0.59) +
+    #geom_line(lwd = 1) +
+    geom_point(alpha = 0.2) +
+    geom_smooth(method = "loess", span = 0.02, se = FALSE) +
+    ggpubr::theme_pubclean(base_size = 14) +
+    theme(axis.line = element_line(), legend.position = "right",
+          legend.key = element_rect(fill = "white")) +
+    scale_color_discrete(name = "Scenario:") +
+    xlab("Days Since Recognition of COVID-19") +
+    ylab("Rt")
+
+  gg
 
 }
